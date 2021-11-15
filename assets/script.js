6c44b4c8af86d2b995ab9e18275990bbcc2bc295
callmeNeo <nbi6731@gmail.com>
2021-11-15 17:54:33 +0700

    Music player

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// Variables===================================================================
 const playBtn = $(".play-btn");
 const nextBtn = $(".next-btn");
 const prevBtn = $(".prev-btn");
@@ -9,6 +10,10 @@ const author = $(".header__song-author");
 const audio = $(".audio");
 const player = $(".player");
 const message = $(".message");
+const cdImg = $(".cd-img");
+const replayBtn = $(".replay-btn");
+
+// ===========================================================================
 
 const app = {
   currentIndex: 0,
@@ -18,42 +23,49 @@ const app = {
       author: "Ngọt",
       path: "./assets/musics/Lan Cuoi.mp3",
       message: "Nhớ ăn uống đầy đủ nheee emmm <3",
+      imgPath: "./assets/images/lancuoi.jpg",
     },
     {
       name: "Here Without You",
       author: "3 Doors Down",
       path: "./assets/musics/Here without you.mp3",
       message: "Hát đám cưới nhe kkk ^^",
+      imgPath: "./assets/images/here without u.jpg",
     },
     {
       name: "Tình đắng như ly Cafe",
       author: "nân. x Ngơ",
       path: "./assets/musics/Tình đắng như ly cafe.mp3",
       message: "Luv Ya So <33",
+      imgPath: "./assets/images/tinh dang nhu ly cafe.jpg",
     },
     {
       name: "Bước qua mùa cô đơn",
       author: "Vũ",
       path: "./assets/musics/buoc qua mua co don.mp3",
       message: "Ngủ nhiều nhe, ngủ nhiều anh mới thương (◠‿◠)",
+      imgPath: "./assets/images/buoc qua mua co don.jpg",
     },
     {
       name: "Đi qua mùa hạ",
       author: "Thái Đinh",
       path: "./assets/musics/di qua mua ha.mp3",
       message: "Iu cô bé mặt măm (っ◔◡◔)っ ♥ ",
+      imgPath: "./assets/images/di qua mua ha.jpg",
     },
     {
       name: "Mascara",
       author: "Chilles",
       path: "./assets/musics/mascara.mp3",
       message: "Love u til i die  ｡◕‿◕｡",
+      imgPath: "./assets/images/mascara.jpg",
     },
   ],
   getCurrentSong: function () {
     return this.songs[this.currentIndex];
   },
 
+  // Next song Function ===========================================================
   nextSong: function () {
     if (this.currentIndex >= this.songs.length - 1) {
       this.currentIndex = 0;
@@ -64,6 +76,7 @@ const app = {
     }
   },
 
+  // Prev song Function========================================================
   prevSong: function () {
     if (this.currentIndex > 0) {
       this.currentIndex--;
@@ -74,24 +87,26 @@ const app = {
     }
   },
 
+  // Handle Events=================================================================
   handleEvents: function () {
     const _this = this;
     let isPlay = false;
+    let isRepeat = false;
 
-    // Next song
+    // Next song========================================================================
     nextBtn.onclick = function () {
       console.log(_this.currentIndex, _this.songs.length);
       _this.nextSong();
       audio.play();
     };
 
-    // Prev song
+    // Prev song========================================================================
     prevBtn.onclick = function () {
       _this.prevSong();
       audio.play();
     };
 
-    //Play song
+    //Play song========================================================================
     playBtn.onclick = function () {
       if (isPlay) {
         audio.pause();
@@ -100,7 +115,13 @@ const app = {
       }
     };
 
-    //Audio Status
+    // Repeat song========================================================================
+    replayBtn.onclick = function () {
+      isRepeat = true;
+      replayBtn.classList.toggle("pressed");
+    };
+
+    //Audio Status===============================================================
     audio.onpause = function () {
       isPlay = false;
       player.classList.remove("playing");
@@ -109,15 +130,24 @@ const app = {
       isPlay = true;
       player.classList.add("playing");
     };
+    audio.onended = function () {
+      if (isRepeat) {
+        audio.play();
+      } else {
+        nextBtn.click();
+      }
+    };
   },
 
+  // Render songs ================================================================
   render: function () {
     title.textContent = this.songs[this.currentIndex].name;
     author.textContent = this.songs[this.currentIndex].author;
     audio.src = this.songs[this.currentIndex].path;
     message.textContent = this.songs[this.currentIndex].message;
+    cdImg.src = this.songs[this.currentIndex].imgPath;
   },
-
+  // Start======================================================================
   start: function () {
     this.render();
     this.handleEvents();
